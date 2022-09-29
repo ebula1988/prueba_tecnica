2da29768f53c9a433dcdec9f629577c87db4d867
Edgar Romero Bula <edgaromerobula@gmail.com>
2022-09-29 15:43:53 -0500

    pruena tecnica

---
+++ src/components/ListPeliculasYear.js
@@ -0,0 +1,68 @@
+import React, { useState, useEffect } from 'react'
+
+const ListPeliculasYear = () => {
+    const [peliculas, setPeliculas] = useState([])
+
+    const numeroTotal = 6
+    let tmpPeliculas = [];
+
+
+    useEffect(() => {
+        getPeliculas();
+    }, [])
+
+    const groupBy = key => array =>
+        array.reduce(
+            (objectsByKeyValue, obj) => ({
+                ...objectsByKeyValue,
+                [obj[key]]: (objectsByKeyValue[obj[key]] || []).concat(obj)
+            }),
+            {}
+        );
+
+    const getPeliculas = async () => {
+        tmpPeliculas = []; //limpiar las peliculas
+
+        for (let i = 1; i <= numeroTotal; i++) {
+
+            const data = await fetch('https://jsonmock.hackerrank.com/api/movies?page=' + i + '')
+
+            const peliculasApi = await data.json() // array de objetos por pagina
+
+            for(let y = 0; y<peliculasApi.data.length; y++){
+                tmpPeliculas.push(peliculasApi.data[y]);
+            }
+
+        }
+
+        const groupByYear = groupBy('Year');
+        const peliculasAgrupadas = groupByYear(tmpPeliculas);
+        setPeliculas(peliculasAgrupadas);
+        console.log('peli:',peliculasAgrupadas);
+        
+        
+    }
+ 
+
+
+    
+    return (
+        <>
+        {
+        }
+
+      
+        </>
+
+    );
+        
+        
+       
+
+    
+};
+
+
+
+
+export default ListPeliculasYear
